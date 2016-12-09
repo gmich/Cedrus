7bb604daef054449a75bdfd6ed1d9f51c67fec7f
George Michaelides <g.mich90@gmail.com>
2016-12-09 12:21:46 +0200

    Added tandrix clone project in playground

---
+++ Cedrus.Playground/Gmich.Cedrus.TandrixClone/Program.cs
@@ -0,0 +1,18 @@
+﻿using Gmich.Cedrus.IOC;
+using System;
+
+namespace Gmich.Cedrus.TandrixClone
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var builder = new IocBuilder();
+            using (var game = new CedrusGame(builder))
+            {
+                game.Run();
+            }
+        }
+    }
+}
